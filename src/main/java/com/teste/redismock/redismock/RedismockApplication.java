ce98893cac1146ca2ace238bbe87a31e7587fb93
Gustavo Kring <gustavoktausend@gmail.com>
2020-01-06 17:26:18 -0300

    alterando configurações de conexão

--- src/main/java/com/teste/redismock/redismock/RedismockApplication.java
+++ src/main/java/com/teste/redismock/redismock/RedismockApplication.java
@@ -9,25 +9,11 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 
-@Configuration
 @SpringBootApplication
 public class RedismockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedismockApplication.class, args);
-	}
-
-	@Bean
-	JedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory();
-	}
-
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate() {
-		final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-		template.setConnectionFactory(jedisConnectionFactory());
-		template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-		return template;
 	}
 
 }
